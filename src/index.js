8829849b6370b89e53bb4bab01dd09eb86e76286
Yvens16 <yvensbelaston@gmail.com>
2020-11-05 13:27:38 +0100

    initialize project with firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import FirebaseProvider from './utils/firebase';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/utils/firebase.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { createContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/analytics";
+import "firebase/functions";
+
+const FirebaseContext = createContext(null);
+export { FirebaseContext };
+
+export default (({ children }) => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    });
+  }
+  if (process.env.NODE_ENV === "development") {
+    firebase.functions().useFunctionsEmulator("http://localhost:5001");
+  }
+  return (
+    <FirebaseContext.Provider value={firebase}>
+      {children}
+    </FirebaseContext.Provider>
+  )
+})
